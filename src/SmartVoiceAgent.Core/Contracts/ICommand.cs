c41ff50aacf12467a02c1db46047ac7f342970e5
Furkan <esquetta@gmail.com>
2025-05-25 19:15:29 +0300

    Code refactoring.

--- src/SmartVoiceAgent.Core/Contracts/ICommand.cs
+++ src/SmartVoiceAgent.Core/Contracts/ICommand.cs
@@ -4,10 +4,4 @@
     /// Marker interface for commands.
     /// </summary>
     public interface ICommand { }
-
-    /// <summary>
-    /// Marker interface for queries with a response type.
-    /// </summary>
-    /// <typeparam name="TResult"></typeparam>
-    public interface IQuery<TResult> { }
 }
