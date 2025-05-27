b05b94ec5f4abce40d9ca66b4c164414e140379b
Furkan <esquetta@gmail.com>
2025-05-27 10:30:31 +0300

    Busses added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/ICommandBus.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartVoiceAgent.Core.Interfaces;
+
+/// <summary>
+/// Dispatches commands to their corresponding handlers.
+/// </summary>
+public interface ICommandBus
+{
+    /// <summary>
+    /// Dispatches the specified command to its handler asynchronously.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of command.</typeparam>
+    /// <typeparam name="TResult">The type of result expected.</typeparam>
+    /// <param name="command">The command instance to dispatch.</param>
+    /// <returns>The result of command execution.</returns>
+    Task<TResult> SendAsync<TCommand, TResult>(TCommand command);
+}
