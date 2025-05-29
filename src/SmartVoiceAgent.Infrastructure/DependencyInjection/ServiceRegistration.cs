06e926370a398fff399c86872383c2bb40a9d60a
Furkan <esquetta@gmail.com>
2025-05-29 20:29:24 +0300

    Service registration added.

---
+++ src/SmartVoiceAgent.Infrastructure/DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SmartVoiceAgent.Core.Interfaces;
+using SmartVoiceAgent.Infrastructure.Services;
+
+namespace SmartVoiceAgent.Infrastructure.DependencyInjection;
+
+/// <summary>
+/// Service registrations for Infrastructure layer.
+/// </summary>
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IApplicationService, ApplicationService>();
+        services.AddScoped<IVoiceRecognitionService, VoiceRecognitionService>();
+
+        return services;
+    }
+}
