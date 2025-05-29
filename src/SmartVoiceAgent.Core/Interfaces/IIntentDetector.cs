d3526d547492dfdc11caf6b1734c0c5a8da4b443
Furkan <esquetta@gmail.com>
2025-05-29 20:29:04 +0300

    IIntentDetector added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/IIntentDetector.cs
@@ -0,0 +1,11 @@
+﻿using SmartVoiceAgent.Core.Enums;
+
+public interface IIntentDetector
+{
+    /// <summary>
+    /// Detects the intent from a given voice input asynchronously.
+    /// </summary>
+    /// <param name="voiceInput">The voice command input as text.</param>
+    /// <returns>The detected command type.</returns>
+    Task<CommandType> DetectIntentAsync(string voiceInput);
+}
