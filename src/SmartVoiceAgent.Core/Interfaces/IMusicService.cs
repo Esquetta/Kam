a2c608d791e1d6342e49108a7efd886cac17951e
Furkan <esquetta@gmail.com>
2025-06-02 20:16:28 +0300

    Music service added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/IMusicService.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartVoiceAgent.Core.Interfaces;
+
+/// <summary>
+/// Service interface for music operations.
+/// </summary>
+public interface IMusicService
+{
+    /// <summary>
+    /// Plays a music track asynchronously.
+    /// </summary>
+    /// <param name="trackName">The name of the track to play.</param>
+    Task PlayMusicAsync(string trackName);
+}
---
+++ src/SmartVoiceAgent.Infrastructure/Services/MusicService.cs
@@ -0,0 +1,15 @@
+﻿using SmartVoiceAgent.Core.Interfaces;
+
+namespace SmartVoiceAgent.Infrastructure.Services;
+
+/// <summary>
+/// Dummy implementation for music service.
+/// </summary>
+public class MusicService : IMusicService
+{
+    public Task PlayMusicAsync(string trackName)
+    {
+        // TODO: Müzik çalma entegrasyonu yapılacak.
+        return Task.CompletedTask;
+    }
+}
