bf181764c0123f6df43ff0b7147c95835b22b38e
Furkan <esquetta@gmail.com>
2025-06-02 19:56:02 +0300

    Test console.added.

---
+++ src/SmartVoiceAgent.AgentHost.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+using SmartVoiceAgent.Application.DependencyInjection;
+using SmartVoiceAgent.Infrastructure.DependencyInjection;
+
+var builder = Host.CreateDefaultBuilder(args).ConfigureServices((hostContext, services) =>
+{
+    services.AddApplicationServices();
+    services.AddInfrastructureServices();
+});
+
+var host = builder.Build();
+await host.RunAsync();
