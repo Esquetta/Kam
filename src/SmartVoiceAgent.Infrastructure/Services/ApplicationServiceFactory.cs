5f8f707c3b651804fd71a653b160b04e68a0c459
Furkan <esquetta@gmail.com>
2025-06-21 11:39:53 +0300

    Service factory added.

---
+++ src/SmartVoiceAgent.Infrastructure/Services/ApplicationServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿using SmartVoiceAgent.Core.Interfaces;
+using SmartVoiceAgent.Infrastructure.Services.Application;
+
+namespace SmartVoiceAgent.Infrastructure.Services
+{
+    public static class ApplicationServiceFactory
+    {
+        public static IApplicationService Create()
+        {
+            if (OperatingSystem.IsWindows())
+                return new WindowsApplicationService();
+            if (OperatingSystem.IsLinux())
+                return new LinuxApplicationService();
+            // if (OperatingSystem.IsMacOS())
+            //     return new MacOsApplicationService();
+
+            throw new PlatformNotSupportedException("Unsupported OS platform.");
+        }
+    }
+}
