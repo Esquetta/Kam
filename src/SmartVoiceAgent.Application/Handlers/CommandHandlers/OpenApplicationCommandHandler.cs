55d0a3180daaec26c0da52e5e3890bb05eecf79d
Furkan <esquetta@gmail.com>
2025-06-24 20:53:07 +0300

    ApplicationService added.

--- src/SmartVoiceAgent.Application/Handlers/CommandHandlers/OpenApplicationCommandHandler.cs
+++ src/SmartVoiceAgent.Application/Handlers/CommandHandlers/OpenApplicationCommandHandler.cs
@@ -3,20 +3,25 @@ using SmartVoiceAgent.Application.NotificationHandlers;
 using SmartVoiceAgent.Core.Commands;
 using SmartVoiceAgent.Core.Interfaces;
 
-namespace SmartVoiceAgent.Application.Handlers.Commands;
-
+namespace SmartVoiceAgent.Application.Handlers;
 /// <summary>
 /// Handles the command to open an application.
 /// </summary>
-public sealed class OpenApplicationCommandHandler(IApplicationService appService, IMediator _mediator) : IRequestHandler<OpenApplicationCommand, CommandResultDTO>
+public sealed class OpenApplicationCommandHandler : IRequestHandler<OpenApplicationCommand, CommandResultDTO>
 {
-    private readonly IApplicationService _appService = appService;
     private readonly IMediator _mediator;
-
+    private readonly IApplicationServiceFactory _factory;
+    public OpenApplicationCommandHandler(IMediator mediator, IApplicationServiceFactory factory)
+    {
+        _mediator = mediator;
+        _factory = factory;
+    }
 
     public async Task<CommandResultDTO> Handle(OpenApplicationCommand request, CancellationToken cancellationToken)
     {
-        // TODO: Implement device control logic.
+
+        var appService = _factory.Create();
+        await appService.OpenApplicationAsync(request.ApplicationName);
         await _mediator.Publish(new ApplicationOpenedNotification(request.ApplicationName), cancellationToken);
         await Task.CompletedTask;
         return new CommandResultDTO(true, $"{request.ApplicationName} application starting...");
