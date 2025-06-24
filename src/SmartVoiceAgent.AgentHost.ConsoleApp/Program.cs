a66c026ba88afe4c1e0c3269068df6362919dd4b
Furkan <esquetta@gmail.com>
2025-06-24 20:52:53 +0300

    Code refactoring.

--- src/SmartVoiceAgent.AgentHost.ConsoleApp/Program.cs
+++ src/SmartVoiceAgent.AgentHost.ConsoleApp/Program.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using SmartVoiceAgent.Application.DependencyInjection;
 using SmartVoiceAgent.Infrastructure.DependencyInjection;
 
 
 var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {       
+        config.AddUserSecrets<Program>();
+    })
     .ConfigureServices((context, services) =>
     {
         services.AddApplicationServices();
