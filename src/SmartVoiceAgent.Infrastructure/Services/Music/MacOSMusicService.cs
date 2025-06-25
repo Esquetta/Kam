23389311aad0abaa8409dddd9dadedf8d8dfe62a
Furkan <esquetta@gmail.com>
2025-06-25 19:27:22 +0300

    Music Services added.

---
+++ src/SmartVoiceAgent.Infrastructure/Services/Music/MacOSMusicService.cs
@@ -0,0 +1,22 @@
+﻿using SmartVoiceAgent.Core.Interfaces;
+using System.Diagnostics;
+
+namespace SmartVoiceAgent.Infrastructure.Services.Music
+{
+    public class MacOSMusicService : IMusicService
+    {
+        private Process? _musicProcess;
+
+        public Task PlayMusicAsync(string filePath)
+        {
+            _musicProcess = Process.Start("open", filePath);
+            return Task.CompletedTask;
+        }
+
+        public Task StopMusicAsync()
+        {
+            _musicProcess?.Kill();
+            return Task.CompletedTask;
+        }
+    }
+}
