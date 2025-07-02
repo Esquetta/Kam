bec594a6532760dacf3e11bc9b9e150bbd1568a3
Furkan <esquetta@gmail.com>
2025-07-02 19:49:25 +0300

    EventArgs added.

---
+++ src/SmartVoiceAgent.Core/EventArgs/PipelineErrorEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartVoiceAgent.Core.EventArgs;
+
+public class PipelineErrorEventArgs : System.EventArgs
+{
+    public Exception Exception { get; set; }
+    public string Stage { get; set; } // "STT", "LanguageDetection", "IntentDetection"
+    public string Context { get; set; }
+    public Guid SessionId { get; set; }
+}
---
+++ src/SmartVoiceAgent.Core/EventArgs/VoiceProcessedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using SmartVoiceAgent.Core.Entities;
+
+namespace SmartVoiceAgent.Core.EventArgs;
+
+public class VoiceProcessedEventArgs : System.EventArgs
+{
+    public IntentResult Intent { get; set; }
+    public SpeechResult Speech { get; set; }
+    public LanguageResult Language { get; set; }
+    public TimeSpan TotalProcessingTime { get; set; }
+    public Guid SessionId { get; set; }
+}
