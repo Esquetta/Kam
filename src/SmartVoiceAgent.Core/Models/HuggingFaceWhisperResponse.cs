812b9978df31c680d92dbbf811d225b67b579f56
Furkan <esquetta@gmail.com>
2025-07-06 18:01:47 +0300

    Stt service response model added.

---
+++ src/SmartVoiceAgent.Core/Models/HuggingFaceWhisperResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartVoiceAgent.Core.Models
+{
+    public class HuggingFaceWhisperResponse
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = string.Empty;
+    }
+
+    public class HuggingFaceGenericResponse
+    {
+        [JsonPropertyName("transcription_text")]
+        public string TranscriptionText { get; set; } = string.Empty;
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = string.Empty;
+    }
+
+    public class HuggingFaceErrorResponse
+    {
+        [JsonPropertyName("error")]
+        public string Error { get; set; } = string.Empty;
+
+        [JsonPropertyName("estimated_time")]
+        public int? EstimatedTime { get; set; }
+    }
+}
