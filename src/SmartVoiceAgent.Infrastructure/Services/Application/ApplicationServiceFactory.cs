9789404edb8124e11c0a9c5a14cdfb2fdf6cda55
Furkan <esquetta@gmail.com>
2025-07-11 21:12:26 +0300

    Revert "Code refactor."
    This reverts commit a50497b4836a9dca00f27ea4c39ad45f767b470e.

--- src/SmartVoiceAgent.Infrastructure/Services/Application/ApplicationServiceFactory.cs
+++ src/SmartVoiceAgent.Infrastructure/Services/Application/ApplicationServiceFactory.cs
@@ -1,16 +1,14 @@
 ﻿using SmartVoiceAgent.Core.Interfaces;
 using SmartVoiceAgent.Infrastructure.Services.Application;
 
-public  static class ApplicationServiceFactory 
+public class ApplicationServiceFactory : IApplicationServiceFactory
 {
-    public static IApplicationService Create()
+    public IApplicationService Create()
     {
         if (OperatingSystem.IsWindows())
             return new WindowsApplicationService();
         if (OperatingSystem.IsLinux())
             return new LinuxApplicationService();
-        if (OperatingSystem.IsMacOS())
-            return new MacOSApplicationService();
 
         throw new PlatformNotSupportedException();
     }
