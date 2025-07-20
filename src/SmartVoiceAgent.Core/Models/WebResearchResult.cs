077db1c61a428134c1ad370ce7b8b2b0486a54d0
Furkan <esquetta@gmail.com>
2025-07-20 22:19:36 +0300

    Web Research models added.

---
+++ src/SmartVoiceAgent.Core/Models/WebResearchResult.cs
@@ -0,0 +1,14 @@
+﻿namespace SmartVoiceAgent.Core.Models;
+public class WebResearchResult
+{
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public string Description { get; set; }
+    public DateTime SearchDate { get; set; }
+}
+public class WebResearchRequest
+{
+    public string Query { get; set; }
+    public int MaxResults { get; set; } = 5;
+    public string Language { get; set; } = "tr";
+}
