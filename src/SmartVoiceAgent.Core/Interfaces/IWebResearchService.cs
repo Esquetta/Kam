a3498fc45054e0ce3033819776025d78067dbd26
Furkan <esquetta@gmail.com>
2025-07-20 22:19:46 +0300

    Web Research interface added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/IWebResearchService.cs
@@ -0,0 +1,9 @@
+﻿using SmartVoiceAgent.Core.Models;
+
+namespace SmartVoiceAgent.Core.Interfaces;
+public interface IWebResearchService
+{
+    Task<List<WebResearchResult>> SearchAsync(WebResearchRequest request);
+    Task OpenLinksInBrowserAsync(List<WebResearchResult> results);
+    Task<List<WebResearchResult>> SearchAndOpenAsync(WebResearchRequest request);
+}
