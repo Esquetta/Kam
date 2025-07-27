eefdbe2a60e12fbb9537ac06bea4893b36760bb2
Furkan <esquetta@gmail.com>
2025-07-27 21:54:14 +0300

    Command entity added.

---
+++ src/SmartVoiceAgent.Core/Entities/AvailableCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartVoiceAgent.Core.Entities;
+public class AvailableCommand
+{
+    public string Intent { get; set; }
+    public string Description { get; set; }
+    public List<string> Examples { get; set; }
+    public string Category { get; set; }
+}
