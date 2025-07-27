4a829063591a8b643ee6f41216463b26d24460b5
Furkan <esquetta@gmail.com>
2025-07-27 21:50:32 +0300

    Command entities added.

---
+++ src/SmartVoiceAgent.Core/Entities/CommandResult.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartVoiceAgent.Core.Entities;
+public class CommandResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+    public string Error { get; set; }
+    public string OriginalInput { get; set; }
+
+}
---
+++ src/SmartVoiceAgent.Core/Entities/DynamicCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartVoiceAgent.Core.Entities;
+public class DynamicCommandRequest
+{
+    public string Intent { get; set; }
+    public Dictionary<string, object> Entities { get; set; }
+    public string OriginalText { get; set; }
+    public string Language { get; set; }
+    public string Context { get; set; }
+}
