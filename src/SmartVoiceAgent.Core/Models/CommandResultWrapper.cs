8cfa4fe635bc421af85f64cb8938d6f4c5f55cf3
Furkan <esquetta@gmail.com>
2025-08-10 13:56:21 +0300

    New model added.

---
+++ src/SmartVoiceAgent.Core/Models/CommandResultWrapper.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartVoiceAgent.Core.Models;
+/// <summary>
+/// Wrapper class for command results
+/// </summary>
+public class CommandResultWrapper
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = "";
+    public string Error { get; set; } = "";
+}
