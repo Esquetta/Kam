bdccf2d7fc668b9b4bab9eeca1cdad2e3bf164c7
Furkan <esquetta@gmail.com>
2025-08-15 19:38:35 +0300

    New command added.

---
+++ src/SmartVoiceAgent.Application/Commands/GetApplicationExecutablePathCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SmartVoiceAgent.Application.Pipelines.Caching;
+using SmartVoiceAgent.Application.Pipelines.Performance;
+
+namespace SmartVoiceAgent.Application.Commands
+{
+    public record GetApplicationExecutablePathCommand(string ApplicationName) : IRequest<CommandResultDTO>, ICachableRequest, IIntervalRequest
+    {
+        // Caching
+        public string CacheKey => $"GetApplicationExecutablePath-{ApplicationName}";
+        public string? CacheGroupKey => "GetApplicationExecutablePath";
+        public bool BypassCache => false;
+        public TimeSpan? SlidingExpiration => TimeSpan.FromMinutes(5);
+
+        // Performance
+        public bool EnablePerformanceLogging => true;
+
+        public int Interval => 2;
+    }
+}
---
+++ src/SmartVoiceAgent.Application/Handlers/CommandHandlers/GetApplicationExecutablePathCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using SmartVoiceAgent.Application.Commands;
+using SmartVoiceAgent.Core.Interfaces;
+
+namespace SmartVoiceAgent.Application.Handlers.CommandHandlers
+{
+    public class GetApplicationExecutablePathCommandHandler:IRequestHandler<GetApplicationExecutablePathCommand, CommandResultDTO>
+    {
+        private readonly IMediator _mediator;
+        private readonly IApplicationServiceFactory _factory;
+        public GetApplicationExecutablePathCommandHandler(IMediator mediator, IApplicationServiceFactory factory)
+        {
+            _mediator = mediator;
+            _factory = factory;
+        }
+
+        public async Task<CommandResultDTO> Handle(GetApplicationExecutablePathCommand request, CancellationToken cancellationToken)
+        {
+            var appService = _factory.Create();
+            //await appService(request.ApplicationName);
+            await Task.CompletedTask;
+            return new CommandResultDTO(true, $"{request.ApplicationName} execute path found...");
+        }
+    }
+}
