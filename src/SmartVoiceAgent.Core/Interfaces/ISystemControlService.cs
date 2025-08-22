2ec97e11f85c802753170ce45c000df30895f899
Furkan <esquetta@gmail.com>
2025-08-22 19:43:25 +0300

    System interfaces added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/ISystemControlService.cs
@@ -0,0 +1,32 @@
+﻿using SmartVoiceAgent.Core.Models;
+
+namespace SmartVoiceAgent.Core.Interfaces;
+public interface ISystemControlService
+{
+    Task<bool> SetSystemVolumeAsync(int level);
+    Task<bool> IncreaseSystemVolumeAsync(int step = 10);
+    Task<bool> DecreaseSystemVolumeAsync(int step = 10);
+    Task<bool> MuteSystemVolumeAsync();
+    Task<bool> UnmuteSystemVolumeAsync();
+    Task<int> GetSystemVolumeAsync();
+
+    Task<bool> SetScreenBrightnessAsync(int level);
+    Task<bool> IncreaseScreenBrightnessAsync(int step = 10);
+    Task<bool> DecreaseScreenBrightnessAsync(int step = 10);
+    Task<int> GetScreenBrightnessAsync();
+
+    Task<bool> EnableWiFiAsync();
+    Task<bool> DisableWiFiAsync();
+    Task<bool> GetWiFiStatusAsync();
+
+    Task<bool> EnableBluetoothAsync();
+    Task<bool> DisableBluetoothAsync();
+    Task<bool> GetBluetoothStatusAsync();
+
+    Task<bool> ShutdownSystemAsync(int delayMinutes = 0);
+    Task<bool> RestartSystemAsync(int delayMinutes = 0);
+    Task<bool> SleepSystemAsync();
+    Task<bool> LockSystemAsync();
+
+    Task<SystemStatusInfo> GetSystemStatusAsync();
+}
