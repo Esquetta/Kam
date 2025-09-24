aa2324c4f13173dd2b1ef0f96112271194c8680d
Furkan <esquetta@gmail.com>
2025-09-24 19:36:51 +0300

    Screen capture interfaces added.

---
+++ src/SmartVoiceAgent.Core/Interfaces/IOcrService.cs
@@ -0,0 +1,16 @@
+﻿using SmartVoiceAgent.Core.Dtos;
+using System.Drawing;
+
+namespace SmartVoiceAgent.Core.Interfaces;
+/// <summary>
+/// Provides methods for performing OCR on images.
+/// </summary>
+public interface IOcrService
+{
+    /// <summary>
+    /// Extracts text lines from the given bitmap image.
+    /// </summary>
+    /// <param name="image">The image to process.</param>
+    /// <returns>Collection of OCR line results.</returns>
+    Task<IEnumerable<OcrLine>> ExtractTextAsync(Bitmap image);
+}
---
+++ src/SmartVoiceAgent.Core/Interfaces/IScreenCaptureService.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace SmartVoiceAgent.Core.Interfaces;
+/// <summary>
+/// Provides methods for capturing the screen.
+/// </summary>
+public interface IScreenCaptureService
+{
+    /// <summary>
+    /// Captures the entire screen as a bitmap.
+    /// </summary>
+    /// <returns>Bitmap image of the current screen.</returns>
+    Task<Bitmap> CaptureScreenAsync();
+}
