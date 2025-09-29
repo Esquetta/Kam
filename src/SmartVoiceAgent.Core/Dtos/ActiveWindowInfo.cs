6ffcb6c1891324a5dd895ebc2cd51972bebcecaa
Furkan <esquetta@gmail.com>
2025-09-29 19:19:56 +0300

    Code refactoring.

--- src/SmartVoiceAgent.Core/Dtos/ActiveWindowInfo.cs
+++ src/SmartVoiceAgent.Core/Dtos/ActiveWindowInfo.cs
@@ -1,8 +1,14 @@
-﻿namespace SmartVoiceAgent.Core.Dtos;
+﻿using System.Drawing;
+
+namespace SmartVoiceAgent.Core.Dtos;
 public record ActiveWindowInfo
 {
     public string Title { get; init; }
     public int ProcessId { get; init; }
     public string ProcessName { get; init; }
     public string ExecutablePath { get; init; }
+    public Rectangle WindowBounds { get; init; }
+    public bool IsMaximized { get; init; }
+    public bool IsMinimized { get; init; }
+    public bool IsVisible { get; init; }
 }
