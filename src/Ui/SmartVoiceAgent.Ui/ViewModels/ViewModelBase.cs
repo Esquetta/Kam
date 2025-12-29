c9b7c50b3b04d33097c90bc5f225d079be27f9f3
Furkan <esquetta@gmail.com>
2025-12-29 19:49:59 +0300

    ReactiveUi Migration

--- src/Ui/SmartVoiceAgent.Ui/ViewModels/ViewModelBase.cs
+++ src/Ui/SmartVoiceAgent.Ui/ViewModels/ViewModelBase.cs
@@ -1,8 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using ReactiveUI;
 
 namespace SmartVoiceAgent.Ui.ViewModels
 {
-    public abstract class ViewModelBase : ObservableObject
+    public abstract class ViewModelBase : ReactiveObject
     {
     }
 }
