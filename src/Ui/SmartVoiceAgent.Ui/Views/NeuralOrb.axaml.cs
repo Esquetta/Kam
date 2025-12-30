635e6ac772548fa3fb775f88fa1508e1f1467efd
Furkan <esquetta@gmail.com>
2025-12-30 20:06:33 +0300

    Neural Orb added

--- src/Ui/SmartVoiceAgent.Ui/Views/NeuralOrb.axaml.cs
+++ src/Ui/SmartVoiceAgent.Ui/Views/NeuralOrb.axaml.cs
@@ -1,11 +1,25 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
-namespace SmartVoiceAgent.Ui;
+namespace SmartVoiceAgent.Ui.Views;
 
 public partial class NeuralOrb : UserControl
 {
     public NeuralOrb()
     {
-        
+
+        InitializeComponent();
+
     }
+
+    public static readonly StyledProperty<IBrush> ActiveColorProperty =
+        AvaloniaProperty.Register<NeuralOrb, IBrush>(nameof(ActiveColor));
+
+    public IBrush ActiveColor
+    {
+        get => GetValue(ActiveColorProperty);
+        set => SetValue(ActiveColorProperty, value);
+    }
+
 }
