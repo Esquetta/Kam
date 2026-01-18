7d43acea8566db61cbd63e6643f3c39cf62d0b61
Furkan <esquetta@gmail.com>
2026-01-18 13:01:46 +0300

    Tray icon added.

--- src/Ui/SmartVoiceAgent.Ui/App.axaml.cs
+++ src/Ui/SmartVoiceAgent.Ui/App.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using SmartVoiceAgent.Ui.ViewModels;
 using SmartVoiceAgent.Ui.Views;
+using System;
 using System.Linq;
 
 namespace SmartVoiceAgent.Ui
@@ -42,6 +43,36 @@ namespace SmartVoiceAgent.Ui
             foreach (var plugin in dataValidationPluginsToRemove)
             {
                 BindingPlugins.DataValidators.Remove(plugin);
+            }
+        }
+        private void ShowWindow_Click(object? sender, EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                if (desktop.MainWindow != null)
+                {
+                    desktop.MainWindow.Show();
+                    desktop.MainWindow.WindowState = Avalonia.Controls.WindowState.Normal;
+                    desktop.MainWindow.Activate();
+                }
+            }
+        }
+
+        private void Settings_Click(object? sender, EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow?.Show();
+                desktop.MainWindow?.Activate();
+                // TODO: Settings view'e navigate et
+            }
+        }
+
+        private void Exit_Click(object? sender, EventArgs e)
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
             }
         }
     }
