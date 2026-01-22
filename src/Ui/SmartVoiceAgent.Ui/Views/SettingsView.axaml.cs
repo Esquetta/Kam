293df0389435d2d3aa10f4dd9aa5d7889abcff04
Furkan <esquetta@gmail.com>
2026-01-22 20:46:32 +0300

    Views added.

---
+++ src/Ui/SmartVoiceAgent.Ui/Views/SettingsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SmartVoiceAgent.Ui.Views;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
