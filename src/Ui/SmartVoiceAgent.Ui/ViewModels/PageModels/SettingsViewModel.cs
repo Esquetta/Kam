97039f03a1c285e3296379fe5c221de5192ab340
Furkan <esquetta@gmail.com>
2026-01-25 23:15:49 +0300

    Add language selection to SettingsViewModel
    Added SelectedLanguageIndex property to SettingsViewModel for language selection. Changing the index updates the app language via LocalizationService. Integrated ReactiveUI for property change notifications. Default language is set to English.

--- src/Ui/SmartVoiceAgent.Ui/ViewModels/PageModels/SettingsViewModel.cs
+++ src/Ui/SmartVoiceAgent.Ui/ViewModels/PageModels/SettingsViewModel.cs
@@ -1,10 +1,45 @@
-﻿namespace SmartVoiceAgent.Ui.ViewModels.PageModels
+﻿using ReactiveUI;
+using SmartVoiceAgent.Ui.Services;
+
+namespace SmartVoiceAgent.Ui.ViewModels.PageModels
 {
     public class SettingsViewModel : ViewModelBase
     {
+        private int _selectedLanguageIndex;
+
         public SettingsViewModel()
         {
             Title = "SETTINGS";
+            _selectedLanguageIndex = 0; // Default to English
+        }
+
+        public int SelectedLanguageIndex
+        {
+            get => _selectedLanguageIndex;
+            set
+            {
+                if (_selectedLanguageIndex != value)
+                {
+                    this.RaiseAndSetIfChanged(ref _selectedLanguageIndex, value);
+                    UpdateLanguage();
+                }
+            }
+        }
+
+        private void UpdateLanguage()
+        {
+            string langCode = _selectedLanguageIndex switch
+            {
+                0 => "en-US",
+                1 => "es-ES",
+                2 => "fr-FR",
+                3 => "de-DE",
+                4 => "zh-CN",
+                5 => "ja-JP",
+                6 => "tr-TR",
+                _ => "en-US"
+            };
+            LocalizationService.Instance.SetLanguage(langCode);
         }
     }
 }
